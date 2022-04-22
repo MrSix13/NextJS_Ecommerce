a68c922b54b33d5b24ff658ec75dbd4a62721a27
MrSix13 <bastiangarcesga@gmail.com>
2022-04-21 22:02:52 -0400

    loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,27 @@
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products'
-import { initialData } from '../database/products'
-import { IProduct } from '../interfaces'
+import { useProducts } from '../hooks'
+import { FullScreenLoading } from '../components/ui'
 
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
+   const {products, isLoading} =useProducts('/products')
+
   return (
     <ShopLayout title={'Ecommerce-Shop- Home'} pageDescription={'Encuentra los mejores productos aca'}>
       <Typography variant='h1' component='h1'>Home</Typography>
       <Typography variant='h2' sx={{mb:1}}>Todos los productos</Typography>
 
-      <ProductList products={initialData.products as any}/>
+      {
+        isLoading
+           ? <FullScreenLoading/>
+           : <ProductList products={products}/>
+      }
 
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
